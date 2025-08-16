16b038cf1e8ab7d1e24ba1108fb36db0b2c2fc8b
Alexandros Bostanidis <alexandrosbostanidis09@gmail.com>
2025-08-16 16:39:14 +0300

    ADded ton connect provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "next-themes";
 import { Theme } from "@radix-ui/themes";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import { tonConnectUI } from "./lib/ton-connect-react";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-    <Theme>
+  <TonConnectUIProvider connector={tonConnectUI}>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <Theme>
         <App />
-    </Theme>
-  </ThemeProvider>
+      </Theme>
+    </ThemeProvider>
+  </TonConnectUIProvider>
 );
