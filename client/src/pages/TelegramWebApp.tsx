6d3de6f9f8b571981c3340f7a9a80c64264c5896
Alexandros Bostanidis <alexandrosbostanidis09@gmail.com>
2025-08-21 11:14:36 +0300

    Max height restriction

--- client/src/pages/TelegramWebApp.tsx
+++ client/src/pages/TelegramWebApp.tsx
@@ -181,7 +181,7 @@ export default function TelegramWebApp() {
       <div className="logo-header h-16 px-3 mb-6">
         <div className="max-w-sm mx-auto flex items-center justify-between h-full">
           <div className="flex items-center max-w-[60%]">
-            <img src="/logo2.png" alt="Showpls" className="block object-contain mr-2 select-none" />
+            <img src="/logo2.png" alt="Showpls"  className="block h-full max-h-16 object-contain mr-2 select-none" />
           </div>
           <div className="flex items-center space-x-2">
             <WalletConnect />
